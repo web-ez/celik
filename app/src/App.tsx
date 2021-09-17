9176cf4fea85c3f626eec57c6101d4ccb54ba09e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 23:39:23 +0200

    Demo updated

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import useBridgeContext from "./util/use-bridge-ctx";
 import { useSmartcard } from "./util/use-smartcard";
@@ -6,6 +6,10 @@ import { useSmartcard } from "./util/use-smartcard";
 function App() {
   const celikCtx = useBridgeContext("celik");
   const { device, devices, card, pickDevice } = useSmartcard();
+
+  const [selectedDevice, setSelectedDevice] = useState("");
+
+  console.log("Loaded.");
 
   useEffect(() => {
     if (!card) return;
@@ -20,20 +24,28 @@ function App() {
     });
   }, [card, device, celikCtx]);
 
-  const clickHandle = () => pickDevice(devices[0]);
+  const clickHandle = () => pickDevice(selectedDevice);
 
   return (
     <div>
       <h1>Hello World!</h1>
       <p>Bemis</p>
       <p>Available Devices: {devices}</p>
+      <select onChange={(e) => setSelectedDevice(e.target.value)}>
+        {devices.map((d, i) => (
+          <option key={i} value={d}>
+            {d}
+          </option>
+        ))}
+        <option value="XD">XD</option>
+      </select>
       <br />
       <br />
       <p>
         Device: {device} <br />
         Card: {card}
       </p>
-      <button onClick={clickHandle}>Pick Device [0]</button>
+      <button onClick={clickHandle}>Pick 1st Device</button>
     </div>
   );
 }
